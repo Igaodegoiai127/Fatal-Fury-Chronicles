f5fbe77d48560766d6091c500e2a7f4155dbf2f6
dcurrent <dc@caskeys.com>
2018-08-21 16:34:56 -0400

    Create dc_draw library from draw0001 remnants.

---
+++ data/scripts/dc_draw/config.h
@@ -0,0 +1,11 @@
+#ifndef DC_DRAW
+
+#define DC_DRAW 1
+
+#define DC_AUTOSCALE_ADJUSTMENT		1.1
+#define MIN_Z_SIZE					0.9		// Size adjustment when at MIN_Z position.
+
+#define DC_DRAW_AUTOSCALE_SCALE_X	256
+#define DC_DRAW_AUTOSCALE_SCALE_Y	256
+
+#endif // !DC_AUTOSCALE
---
+++ data/scripts/dc_draw/main.c
@@ -0,0 +1,3 @@
+#include "data/scripts/dc_draw/config.h"
+
+#import "data/scripts/dc_draw/z_position_autoscale.c"
