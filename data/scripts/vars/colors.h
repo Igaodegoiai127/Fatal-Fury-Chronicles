876c7eb0c7d6e2bee17c0cb7a10f7110d225974d
dcurrent <dc@caskeys.com>
2018-08-22 09:51:47 -0400

    Replace static int with macro.

--- data/scripts/vars/colors.h
+++ data/scripts/vars/colors.h
@@ -1,6 +1,14 @@
-//*****Entity Color Maps*****
-int MAPKO       = 1;									//KO.
-int MAPBURN     = 2;									//Burn.
-int MAPSHOCK    = 3;									//Shock.
-int MAPFREEZ    = 4;									//Freeze.
-int MAPPOIS     = 5;									//Poison.
+#ifndef DC_ENTITY_COLORS
+
+#define DC_ENTITY_COLORS
+
+#define MAPKO		1	// KO.
+#define MAPBURN		2	// Burn.
+#define MAPSHOCK	3	// Shock.
+#define MAPFREEZ	4	// Freeze.
+#define MAPPOIS		5	// Poison.
+
+#endif // !DC_ENTITY_COLORS
+
+
+
