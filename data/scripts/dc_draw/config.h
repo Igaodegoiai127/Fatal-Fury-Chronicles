d5204c1b8266a8c7b4d6ea4480ee4e690321eab5
dcurrent <dc@caskeys.com>
2018-08-22 16:15:05 -0400

    Add DC_DRAW_DEFAULT_SCALE macro.

--- data/scripts/dc_draw/config.h
+++ data/scripts/dc_draw/config.h
@@ -2,6 +2,8 @@
 
 #define DC_DRAW 1
 
+#define DC_DRAW_DEFAULT_SCALE		256		// Value used by drawmethod scale values for 100% size.
+
 #define DC_DRAW_MIN_Z_ADJUST		1.1		// Adjustment from actual min Z position before calculations.
 #define DC_DRAW_MIN_Z_SIZE			0.9		// Scale ratio when target is at MIN_Z position.
 
