76c6bdbe7955f1ec5e53513a0482025fca80ce00
dcurrent <dc@caskeys.com>
2018-08-25 11:29:37 -0400

    Remove bind0007 function and file.

--- data/scripts/deat0002.c
+++ data/scripts/deat0002.c
@@ -1,5 +1,4 @@
 #include "data/scripts/vars/colors.h"
-#include "data/scripts/com/bind0007.h"
 #include "data/scripts/com/bind0008.h"
 
 //Object death, spawns fire entity. Primarily for boats.
@@ -9,8 +8,9 @@ void main()
 	void vSelf = getlocalvar("self");
 	void vSpawn; 
 	
-    vSpawn = bind0007("effe0001", "effe0001", MAPBURN, 1, 0, 50, 3, 0, openborconstant("ANI_FOLLOW12"), 0, 0, 0);       //Spawn fire.
-    changeentityproperty(vSpawn, "setlayer", -7);                                                                       //Set fire layer.
+	//  Fire effect - 2018-08-25, formerly spawned with bind0007. The function spawned and then ran a bind.
+	vSpawn = bind0008("effe0001", "effe0001", MAPBURN, 1, 0, 50, 3, 0, openborconstant("ANI_FOLLOW12"), 0, 0); 
+	changeentityproperty(vSpawn, "setlayer", -7);
 
     vSpawn = bind0008("effe0001", "effe0001", 11, 1, 0, 40, 0, 0, openborconstant("ANI_FOLLOW2"), 0, 1);                //Spawn ball explosion.
 	changeentityproperty(vSpawn, "setlayer", -7);                                                                       //Set ball explosion layer.
