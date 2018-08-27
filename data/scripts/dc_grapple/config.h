c334e89b42dcf38ad396eb320ca5e04a3226637a
dcurrent <dc@caskeys.com>
2018-08-27 15:41:21 -0400

    Create grapple library template.

---
+++ data/scripts/dc_grapple/config.h
@@ -0,0 +1,6 @@
+// From old bind0010
+#include "data/scripts/vars/anims.h"
+#include "data/scripts/vars/entity.h"
+#include "data/scripts/dc_draw/main.c"
+#include "data/scripts/vars/frames.h"
+// -From old bind0010
---
+++ data/scripts/dc_grapple/main.c
@@ -0,0 +1,3 @@
+#include "data/scripts/dc_grapple/config.h"
+
+#import "data/scripts/dc_grapple/functions.c"
