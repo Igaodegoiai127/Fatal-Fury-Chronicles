720d7f57968b77c0f85ff1ec5726d28be3a1aa29
dcurrent <dc@caskeys.com>
2018-08-27 17:39:08 -0400

    Add constants for release.

--- data/scripts/dc_grapple/config.h
+++ data/scripts/dc_grapple/config.h
@@ -1,3 +1,14 @@
+#ifndef DC_GRAPPLE
+
+#define DC_GRAPPLE 1
+
+#define DC_GRAPPLE_RESET_DROP	100		// Knockdown applied to entites auto released from bind.
+#define DC_GRAPPLE_RESET_FORCE	0		// Damage force applied to entites auto released from bind.
+
+
+#endif // !DC_GRAPPLE
+
+
 // From old bind0010
 #include "data/scripts/vars/anims.h"
 #include "data/scripts/vars/entity.h"
