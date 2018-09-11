e0870b174bee1b14681a5c89b1b7f518601af6ed
dcurrent <dc@caskeys.com>
2018-09-11 06:30:50 -0400

    Refine dc_combo_alt() to use localvars with default values for chance and combostep minimum.

--- data/chars/ray/scripts/animation.c
+++ data/chars/ray/scripts/animation.c
@@ -18,32 +18,54 @@ Ray McDougal (AI)
 // and combo has at least one hit in it, then randomly perform
 // an alternate. This is so we can give a bit of variety 
 // to combo finishers for AI.
-void dc_combo_alt(float chance, int animation)
+void dc_combo_alt(int animation)
 {
-    #define COMBO_STEP_MIN 1
+    #define COMBOSTEP_MIN_DEFAULT 1
+	#define CHANCE_DEFAULT 0.3
 
 	void	ent;
 	int		combostep;
 	int		modulo;
-	int		chance_whole;
+	int		chance;
+	int		combostep_min;
 
 	ent			= getlocalvar("self");
 	combostep	= getentityproperty(ent, "combostep");
 	
+	// Get user value. If there isn't one, fall back
+	// to default.
+	combostep_min = getlocalvar("dc_combo_alt_combostep_min");
+
+	if (typeof(combostep_min) == openborconstant("VT_EMPTY"))
+	{
+		combostep_min = COMBOSTEP_MIN_DEFAULT;
+	}
+
+	// Get user value. If there isn't one, fall back
+	// to default.
+	chance = getlocalvar("dc_combo_alt_chance");
+
+	if (typeof(chance) == openborconstant("VT_EMPTY"))
+	{
+		chance = CHANCE_DEFAULT;
+	}
+
 	// Take our chance percentage and make it whole.
 	chance *= 100;
 
 	// Get a modulo value from random number and our 
 	// chance value. 
-	modulo = rand() % chance_whole;
+	modulo = rand() % chance;
 
 	if (modulo > 0)
 	{
-		if (combostep > COMBO_STEP_MIN)
+		if (combostep > combostep_min)
 		{
-			set_entity_property(ent, "animation_animation", animation);
+			changeentityproperty(ent, "animation", animation);
 		}
 	}
 
-	#undef COMBO_STEP_MIN
+	#undef COMBOSTEP_MIN_DEFAULT
+	#undef CHANCE_DEFAULT
+
 }
