f0feeaddcd954e14e652fe8d17fd6444498981ea
dcurrent <dc@caskeys.com>
2018-10-13 14:31:28 -0400

    Remove entity count and other entity variable population from flash spawn script. Restore flash to half size with alpha blend and random rotation.

--- data/scripts/spaw0002.c
+++ data/scripts/spaw0002.c
@@ -1,6 +1,5 @@
 //flash spawn.
 
-#include "data/scripts/vars/entity.h"
 #include "data/scripts/dc_draw/main.c"
 #include "data/scripts/com/soun0001.h"	        //Sound functions.
 #include "data/scripts/com/rnd0001.h"
@@ -8,32 +7,14 @@
 void main(){
 
     void    vSelf   = getlocalvar("self");                                      //Caller.
-    int     iMaxVar = openborvariant("maxentityvars");                          //Maximum vars in use.
-    int     iList   = getindexedvar(0);                                         //Entity counter.
-    int     iCount;                                                             //Loop counter.
-
-    if (!iList)
-    {    
-        iList = 0;  //Count not initalized, set to 0.
-    }
-    else
-    {
-        iList += 1; //Count initalized, increment by 1.
-    }
     
-    setentityvar(vSelf, ENTID, iList);                                          //Set artificial handle.
-    setindexedvar(0, iList);                                                    //Set entity counter.
+	changedrawmethod(vSelf, "scalex", 0.5);
+	changedrawmethod(vSelf, "scaley", 0.5);
+	changedrawmethod(vSelf, "alpha", 1);
+	changedrawmethod(vSelf, "rotate", rnd0001(0, 359, 0, 0, 0, 0));
 
-    for (iCount = 0; iCount <= iMaxVar; iCount++)
-    {
-         setentityvar(vSelf, iCount, NULL());
-    }
-    
-    setentityvar(vSelf, ADSCALEX, 0.5);											//Set half X size.
-    setentityvar(vSelf, ADSCALEY, 0.5);											//Set half Y size.
-    setentityvar(vSelf, ADBLEND, 1);                                            //Set alpha transparency.
-    setentityvar(vSelf, ADROTATE, rnd0001(0, 359,0,0,0,0));                    //Set rotation (rando 0-359).
-    dc_draw_z_position_autoscale(vSelf);                                                            //Apply draw settings.    
+	//Apply draw settings.  
+    dc_draw_z_position_autoscale(vSelf);                                                              
     
 	soun0001(SNDHIT1);
 }
