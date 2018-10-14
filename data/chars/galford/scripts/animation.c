c3d576fea56ee183b895772fe3aa6bafd77db154
dcurrent <dc@caskeys.com>
2018-10-14 14:35:37 -0400

    Clear all non OpenBOR sounds.

---
+++ data/chars/galford/scripts/animation.c
@@ -0,0 +1,30 @@
+/*
+Animation script: 
+
+Andy Bogard
+*/
+
+#include "data/scripts/vars/entity.h"
+
+#include "data/scripts/com/ai0001.h"
+#include "data/scripts/com/ani0003.h"
+#include "data/scripts/com/ani0007.h"
+#include "data/scripts/com/ani0008.h"
+#include "data/scripts/com/bind0002.h"
+#include "data/scripts/com/bind0003.h"
+#include "data/scripts/com/bind0004.h"
+#include "data/scripts/com/bind0008.h"
+#include "data/scripts/com/bind0009.h"
+#include "data/scripts/dc_grapple/main.c"
+#include "data/scripts/dc_sound/main.c"
+
+#include "data/scripts/com/dama0001.h"
+#include "data/scripts/com/hit0001.h"
+#include "data/scripts/com/jump0001.h"
+#include "data/scripts/com/paus0001.h"
+#include "data/scripts/com/proj0002.h"
+#include "data/scripts/com/soun0001.h"
+#include "data/scripts/com/stat0001.h"
+#include "data/scripts/com/summ0001.h"
+#include "data/scripts/com/vars0002.h"
+#include "data/scripts/com/velo0001.h"
