6b0a70625c058cd4f224c701e040876f84719c06
dcurrent <dc@caskeys.com>
2018-10-16 16:20:17 -0400

    Replace spaw0001 with spawnscript_for_level_items.

---
+++ data/scripts/common/spawnscript_for_level_items.c
@@ -0,0 +1,39 @@
+#include "data/scripts/dc_initialize/main.c" 
+
+void main()
+{
+	void ent;
+
+	ent = getlocalvar("self");
+
+	initialize_level_spawn(ent);
+}
+
+void initialize_level_spawn(void ent)
+{
+    void alias; // Caller's alias.
+	int map;	// Caller's map.
+
+	// If entity is spawned into play with its KO map,
+	// then will randomly select one of its other
+	// regular maps here.
+	map = getentityproperty(ent, "map");
+
+    if (map == getentityproperty(ent, "komap"))
+    {   
+		//map = dc_random_color_set(ent);
+
+		changeentityproperty(ent, "map", map);
+    }
+
+    // If the spawned entity is named "random", then we will
+	// generate and assign a random alias here.
+	alias = getentityproperty(ent, "name");
+
+	if (alias == "Random")
+	{
+		alias = dc_random_alias(ent);
+		
+		changeentityproperty(ent, "name", alias);
+	}
+}
