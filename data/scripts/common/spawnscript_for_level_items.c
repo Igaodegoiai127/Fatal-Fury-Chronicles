51e229a3e241af93c19864a0b8fc0ac2fed53d06
dcurrent <dc@caskeys.com>
2018-10-17 15:43:58 -0400

    Move initialize function out to dedicated file for #import.

--- data/scripts/common/spawnscript_for_level_items.c
+++ data/scripts/common/spawnscript_for_level_items.c
@@ -1,5 +1,7 @@
 #include "data/scripts/dc_initialize/main.c" 
 
+#import "data/scripts/common/initialize_level_spawn.c" 
+
 void main()
 {
 	void ent;
@@ -9,31 +11,4 @@ void main()
 	initialize_level_spawn(ent);
 }
 
-void initialize_level_spawn(void ent)
-{
-    void alias; // Caller's alias.
-	int map;	// Caller's map.
 
-	// If entity is spawned into play with its KO map,
-	// then will randomly select one of its other
-	// regular maps here.
-	map = getentityproperty(ent, "map");
-
-    if (map == 1)
-    {   
-		map = dc_random_color_set(ent);
-
-		changeentityproperty(ent, "map", map);
-    }
-
-    // If the spawned entity is named "random", then we will
-	// generate and assign a random alias here.
-	alias = getentityproperty(ent, "name");
-
-	if (alias == "Random")
-	{
-		alias = dc_random_alias(ent);
-		
-		changeentityproperty(ent, "name", alias);
-	}
-}
