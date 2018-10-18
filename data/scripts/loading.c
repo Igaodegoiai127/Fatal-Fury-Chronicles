7a1c794ac3f8d85df92ca02a9c71dedb16e246a0
dcurrent <dc@caskeys.com>
2018-10-18 13:27:08 -0400

    New crop right function, taken from old path0001..

--- data/scripts/loading.c
+++ data/scripts/loading.c
@@ -32,8 +32,15 @@ void main()
 	{
 		list_models_cached();
 	}
+
 }
 
+// Caskey, Damon V.
+// 2018-10-12
+//
+// Experimental function. Just seeing if we
+// can get the path of all models that
+// are known/loaded.
 void list_models_cached()
 {
 	int i;
@@ -43,12 +50,75 @@ void list_models_cached()
 
 	max = openborvariant("models_cached");
 
+	log("\n\n");
+	log("Models List: ");
+	log(max + " found.");
+	log("\n");
+
 	for (i = 0; i < max; i++)
 	{
-		
 		path = getmodelproperty(i, 3);
 
-		log("\npath: " + path);
+		path = dc_string_crop_right(path, "/");
+
+		log("\t");
+		log("Index: " + i);
+		log(", ");
+		log("path: " + path);
+		log("\n");
 	}
+
+	log("\n\n");
 }
+
+// Caskey, Damon V.
+// 2010-06-18
+//
+// Crop the right side of a string (haystack) 
+// starting with last occurence of needle.
+
+// Example: 
+// - haystack:	"data/scripts/hello.c"
+// - needle:	"/"
+// - output:	"data/scripts"
+char dc_string_crop_right(char haystack, char needle)
+{
+	char    temp_string;
+	int     index;
+		
+	// Get string length, minus 1
+	// to account for zero indexing.
+	index = strlength(haystack);
+	
+	// The strinlast function doesn't work, so we'll just build
+	// our own naive search algorithm here. 
+	//
+	// We create a temporary string from haystack, starting
+	// at the far right character. We then use stringinfirst 
+	// to see if our needle is in the temp_string, and if it
+	// is we now have an index to use for strleft. 
+	//
+	// If the result is invalid (-1), then we build the
+	// tempstring from haystack, now one character back, and
+	// continue until our strinfirst gets a good result.
+	do
+	{
+		index--;
+
+		// Get the right end of haystack, starting
+		// from index.
+		temp_string	= strright(haystack, index);
+		
+		log("\n index: " + index + ", temp_string: " + temp_string);
+
+	} while (strinfirst(temp_string, needle) == -1);
+
+	log("\n");
+
+	// Now that we have our index pointing to the last
+	// occurence of needle in haystack, we can use strleft 
+	// to return the preceeding characters.
+	return strleft(haystack, index);
+}
+
 
