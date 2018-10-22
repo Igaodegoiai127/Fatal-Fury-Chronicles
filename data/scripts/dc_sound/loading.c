426fd9352590c828a2c5c1bd978ec07e62eaa1a5
dcurrent <dc@caskeys.com>
2018-10-22 16:33:04 -0400

    Remove dc_sounds_load_array_manual() - this was a 2D array demonstrator now copied to a gist.

--- data/scripts/dc_sound/loading.c
+++ data/scripts/dc_sound/loading.c
@@ -130,10 +130,18 @@ void dc_sounds_setup_ip(char model, int type, char file)
 	// Now we need to locate an empty slot
 	// in the indexes array and load a sound.
 	size = size(indexes);
+	log("\n size (before): " + size);
 
 	add(indexes, size, loadsample(file));
+
+	size = size(indexes);
+	log("\n size (after): " + size);
 }
 
+// Caskey, Damon V.
+// 2018-10-22
+// 
+// Test if a sound loaded properly.
 void dc_sounds_loaded_test(char model, int type, int index)
 {
 	void models;	// Key - model, Value - Sound types array.
@@ -173,85 +181,4 @@ void dc_sounds_loaded_test(char model, int type, int index)
 
 	playsample(index, DC_SOUND_DEFAULT_PRIORITY, DC_SOUND_DEFAULT_VOLUME_LEFT, DC_SOUND_DEFAULT_VOLUME_RIGHT, DC_SOUND_DEFAULT_SPEED, DC_SOUND_DEFAULT_LOOP);
 
-}
-
-void dc_sounds_load_array_manual()
-{
-	void array_cols_a;
-	void array_cols_b;
-	void array_rows;
-	void column_array;
-	void column_element;
-	int  column_last;
-	int  row_last;
-
-	// Column values. We need to create the array of
-	// columns first, since they will be inserted
-	// into a row array element.
-	array_cols_a = array(5);
-	set(array_cols_a, "A", "A");
-	set(array_cols_a, "B", "B");
-	set(array_cols_a, "C", "C");
-	set(array_cols_a, "D", "D");
-	set(array_cols_a, "E", "E");
-
-	array_cols_b = array(5);
-	set(array_cols_b, "A", "Z");
-	set(array_cols_b, "B", "Y");
-	set(array_cols_b, "C", "X");
-	set(array_cols_b, "D", "W");
-	set(array_cols_b, "E", "V");
-
-	// Rows array. Each row element is an array of columns.
-	// Create array.
-	array_rows = array(2);
-	set(array_rows, "0", array_cols_a);
-	set(array_rows, "1", array_cols_b);
-
-	// Reset row cursor.
-	reset(array_rows);
-
-	// Loop all elements of row array.
-	do
-	{
-		// Is this the last row?
-		row_last = islast(array_rows);
-
-		log("Row - key: ");
-		log(key(array_rows));
-		log("\n");
-
-		// Get current row value, which
-		// will be an array of columns.
-		column_array = value(array_rows);
-
-		// Move to cursor to next array
-		// element and get cursor value.
-		// Will be 0 if we're already
-		// at last array element.
-		next(array_rows);
-
-
-		// Reset cursor for our current
-		// array of columns.
-		reset(column_array);
-
-		// Loop all elements of column array.
-		do
-		{
-			column_last = islast(column_array);
-
-			log("Column - Key: ");
-			log(key(column_array));
-
-			column_element = value(column_array);
-			next(column_array);
-
-			log(", value: " + column_element);
-			log("\n");
-
-		} while (!column_last)
-
-			log("\n\n");
-	} while (!row_last);
 }
