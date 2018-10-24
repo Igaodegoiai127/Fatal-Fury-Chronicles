e67f36bdc1a9a959d45b2df2d074400607ed8da5
dcurrent <dc@caskeys.com>
2018-10-24 18:40:45 -0400

    Remove unused functions. Rename (mostly) finihsed sound initialize function. Log sound loading.

--- data/scripts/dc_sound/loading.c
+++ data/scripts/dc_sound/loading.c
@@ -2,87 +2,13 @@
 
 #import "data/scripts/dc_sound/config.c"
 
-void dc_sounds_populate_database(char category, int sample)
-{
-	void array_categories;
-	void array_samples;
-
-	// Array is kept as a localvar
-	// Get the array of sounds.
-	array_categories = getglobalvar("categories_global");
-
-	// Can't do anything else if we didn't get 
-	// an array of categories.
-	if (!array_categories)
-	{
-		return;
-	}
-
-	// Get pointer for samples array stored in this
-	// category element.
-	array_samples = dc_sounds_get_samples_array(array_categories, category);
-}
-
-void dc_sounds_get_categories_array(char identifier)
-{
-	void array_categories;
-
-	// Array is kept as a localvar
-	// Get the array of sounds.
-	array_categories = getglobalvar("categories_global");
-
-	// If there is no category array, then
-	// let's create one and get the pointer.
-	if (!array_categories)
-	{
-		// Create categories array of minimal size.
-		array_categories = array(1);
-	}
-
-}
-
-// Caskey, Damon  V.
-// 2018-10-15
-//
-// Get samples array pointer from category element
-// in categories array. If the category element does
-// not contain a sample array, it will be populated
-// with a new samples array.
-//
-// Returns the samples array pointer
-void dc_sounds_get_samples_array(void categories, char category)
-{
-	void array_samples;
-
-	// Get pointer for samples array stored in this
-	// category element.
-	array_samples = get(categories, category);
-
-	// If there is no samples array in this category, 
-	// then we need to create and use it to populate
-	// this category element.
-	if (!array_samples)
-	{
-		// Create a samples array.  We'll start
-		// with minimal size.
-		array_samples = array(1);
-
-		// Place the pointer for our sample array
-		// into the categories array element for
-		// current category.
-		set(categories, category, array_samples);
-	}
-
-	// Return the samples array pointer.
-	return array_samples;
-}
-
-void dc_sounds_setup_ip(char model, int type, char file)
+void dc_sounds_setup(char model, int type, char file)
 {
 	void models;	// Key - model, Value - Sound types array.
 	void types;		// Key - Sound Type, Value - Sound index array.
 	void indexes;	// Key - Numeric, Value - Sound file index.
-	void index;		// Sound file index.
+	int  sample_id;	// Sample ID loaded to element.
+
 
 	int size;	// Array size.
 	int i;		// Loop index.
@@ -129,15 +55,32 @@ void dc_sounds_setup_ip(char model, int type, char file)
 		set(types, type, indexes);
 	}
 
-	// Now we need to locate an empty slot
-	// in the indexes array and load a sound.
-	size = size(indexes);
-	log("\n size (before): " + size);
+	// Get the array size, we can use this as
+	// the index since we want to add an element.
+	size		= size(indexes);
 
-	add(indexes, size, loadsample(file));
+	// Load the sample and get ID.
+	sample_id	= loadsample(file);
 
-	size = size(indexes);
-	log("\n size (after): " + size);
+	// Add new array element and populate its
+	// value with sample ID.
+	add(indexes, size, sample_id);
+
+	// Output to the log.
+	log("\n Sound sample loaded: ");
+	log("\n");
+	log("\t");
+	log("Model:\t\t" + model);
+	log("\n");
+	log("\t");
+	log("Type:\t\t" + type);
+	log("\n");
+	log("\t");
+	log("Index:\t\t" + size);
+	log("\n");
+	log("\t");
+	log("Sample ID:\t" + sample_id);
+	log("\n");
 }
 
 // Caskey, Damon V.
