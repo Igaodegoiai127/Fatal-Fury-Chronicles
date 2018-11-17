1e09c1f27397ca5e2273e07390988e2cb6ea641a
Caskey, Damon V <dc@caskeys.com>
2018-11-17 14:27:53 -0500

    Remove dc_string to prepare for submodule.

--- data/scripts/dc_string/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef DC_STRING_CONFIG
-
-#define DC_STRING_CONFIG 1
-
-
-#endif // !DC_STRING_CONFIG
-
--- data/scripts/dc_string/main.c
+++
@@ -1,3 +0,0 @@
-#include "data/scripts/dc_string/config.h"
-
-#import "data/scripts/dc_string/crop.c"
