795db60283980cf9457b86dbb48c1ad55c054c2e
Caskey, Damon V <dc@caskeys.com>
2018-11-18 07:34:02 -0500

    Remove dc_math files to prepare for submodule.

--- data/scripts/dc_math/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef DC_MATH_CONFIG
-
-#define DC_MATH_CONFIG 1
-
-#define DC_MATH_VAR_KEY_INSTANCE					"dcmath_0"
-
-#define DC_MATH_DEFAULT_INSTANCE					0
-
-#endif // !DC_MATH_CONFIG
--- data/scripts/dc_math/main.c
+++
@@ -1,5 +0,0 @@
-// General calculations for use in other libraries.
-#include "data/scripts/dc_math/config.h"
-
-#import "data/scripts/dc_math/config.c"
-#import "data/scripts/dc_math/percentage.c"
