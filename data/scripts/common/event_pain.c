948e8c65ee144a2dcee934cbc869b9d4430d9f54
Caskey, Damon V <dc@caskeys.com>
2018-11-20 18:45:52 -0500

    Move pain0001.h to named event script.

--- data/scripts/common/event_pain.c
+++ data/scripts/common/event_pain.c
@@ -1,6 +1,39 @@
-#include "data/scripts/com/pain0001.h"
+#include "data/scripts/dc_kanga/main.c"
 
 void main(){
 
-    pain0001();
+	event_pain_main();
+}
+
+void event_pain_main() {
+
+	void    ent;
+	int     animation;
+	int     lasthit_y;
+	int     height;
+
+	ent = getlocalvar("self");
+	animation = getentityproperty(ent, "animationid");
+
+	if (animation == openborconstant("ANI_BURNPAIN"))
+	{
+	}
+	else if (animation == openborconstant("ANI_SHOCKPAIN"))
+	{
+	}
+	else if (animation == openborconstant("ANI_GRABBED"))
+	{
+	}
+	else
+	{
+		// Hit below 75% of height?
+
+		lasthit_y = openborvariant("lasthita");
+		height = getentityproperty(ent, "height");
+
+		if (lasthit_y < height * 0.75)
+		{
+			changeentityproperty(ent, "animation", openborconstant("ANI_PAIN2"));
+		}
+	}
 }
