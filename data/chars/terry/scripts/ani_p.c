b85a48f9d4a35823f5b74f29f654abe53e916b4d
Caskey, Damon V <dc@caskeys.com>
2019-03-15 20:51:38 -0400

    Add Terry Power Wave.

--- data/chars/terry/scripts/ani_p.c
+++ data/chars/terry/scripts/ani_p.c
@@ -5,5 +5,8 @@ Terry Bogard (PC)
 */
 
 #include "data/scripts/dc_disney/main.c"
+#include "data/scripts/dc_elmers/main.c"
 #include "data/scripts/dc_fidelity/main.c"
+#include "data/scripts/dc_gauntlet/main.c"
 #include "data/scripts/dc_velocity/main.c"
+
