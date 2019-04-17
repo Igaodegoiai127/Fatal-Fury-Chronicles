bbab8e4e49b890d40d6e8896c1a704a62c301bf0
Caskey, Damon V <dc@caskeys.com>
2019-04-17 16:07:48 -0400

    Ray taunts on knockdown.

---
+++ data/chars/ray/scripts/didhitscript.c
@@ -0,0 +1,68 @@
+#include "data/scripts/dc_fidelity/main.c"
+
+void main()
+{
+	dc_common_didhit();
+}
+
+void dc_common_didhit()
+{
+	int blocked;
+	void ent;
+	void target;
+
+	int projectile;
+	int drop_previous;
+	int drop;
+	int animation_id;
+	int knockdown_count;
+	int type;
+
+	blocked = getlocalvar("blocked");
+
+	if (blocked)																			
+	{
+	}
+	else
+	{
+		ent = getlocalvar("self");
+		projectile = getentityproperty(ent, "projectile");
+		
+		animation_id = get_entity_property(ent, "animation_id");
+		
+		target = getlocalvar("damagetaker");
+		drop_previous = get_entity_property(target, "drop");
+
+		// Let's check for a few conditions before we trash talk.
+		// 
+		// 1. Target isn't falling already. We don't want to overdo by talking on juggle hits.
+		// 2. Projectile flag isn't active (i.e. the hit isn't because we weren't thrown into someone).
+		// 3. We're not in a grappled animation.
+		if (!drop_previous && !projectile && animation_id != openborconstant("ANI_FOLLOW10"))
+		{
+			// Will we knock the target down? Todo: Account for target's knockdown defense.
+
+			drop = getlocalvar("drop");
+			knockdown_count = get_entity_property(target, "knockdown_count");
+
+			if (drop > knockdown_count)
+			{
+				// Players trash talk 
+
+				type = getentityproperty(ent, "type");
+
+				if (type == openborconstant("TYPE_PLAYER"))							
+				{
+					dc_fidelity_set_sound_chance(0.5);
+					dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_BATTLE_CRY);
+					dc_fidelity_set_sound_chance(NULL());
+
+				}
+				else
+				{
+					dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_BATTLE_CRY);
+				}
+			}
+		}
+	}
+}
