7e72618312f786e81d4650bfa472857472890bf0
Caskey, Damon V <dc@caskeys.com>
2019-05-02 16:57:34 -0400

    Early stages of grappling for Ryu.

--- data/chars/ryu/script/event_animation_pc.c
+++ data/chars/ryu/script/event_animation_pc.c
@@ -1,4 +1,5 @@
 #include "data/scripts/dc_disney/main.c"
+#include "data/scripts/dc_elmers/main.c"
 #include "data/scripts/dc_gauntlet/main.c"
 #include "data/scripts/dc_fidelity/main.c"
 #include "data/scripts/dc_velocity/main.c"
