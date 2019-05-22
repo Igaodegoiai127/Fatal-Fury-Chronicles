50d5268ab2209bdb60b489e73c84b92d21bcf74b
Caskey, Damon V <dc@caskeys.com>
2019-05-22 09:17:05 -0400

    First test (just compile and load) for Gracie grappling system.

--- data/chars/ryu/script/event_animation_pc.c
+++ data/chars/ryu/script/event_animation_pc.c
@@ -2,6 +2,7 @@
 #include "data/scripts/dc_disney/main.c"
 #include "data/scripts/dc_elmers/main.c"
 #include "data/scripts/dc_gauntlet/main.c"
+#include "data/scripts/dc_gracie/main.c"
 #include "data/scripts/dc_fidelity/main.c"
 #include "data/scripts/dc_velocity/main.c"
 
