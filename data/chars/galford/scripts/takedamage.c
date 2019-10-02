de414005a08a365c1ca470172d9d1d2edf51c0ff
Caskey, Damon V <dc@caskeys.com>
2019-10-02 18:04:15 -0400

    Take damage update in progress to make function more modular.

--- data/chars/galford/scripts/takedamage.c
+++ data/chars/galford/scripts/takedamage.c
@@ -1,44 +1,77 @@
 #include "data/scripts/dc_grapple/main.c"  
 
-/*
-Take damage for Andy Bogard.
-
--Ensure lingering special effects from "C4 in UR Face" throw disappear.
-
--Always fall down and take 50% extra damage if hit out of "C4 in UR Face", 
-"Gourin Kai" (grabforward), "Shiranaui Spider Clutch (grabdown), or 
-"Gekiheki Haisui Shou" (grab up). Extra damage is non lethal.
-
--Run normal take damage functions.
-
-*/
+#include "data/scripts/dc_math/main.c"
 
 void main()
 {
     
     void ent			= getlocalvar("self");                      //Calling entity.    
-    int  force_drop		= getlocalvar("drop");                      //Incomming attack a knockdown?
-    int  iAni			= getentityproperty(ent, "animationid");  //Current animation.
-    int  force_damage	= getlocalvar("damage");                    //Incoming damage.
-    int  health			= getentityproperty(ent, "health");       //Current HP.
+    
 
-	void weapon_item;
-	char weapon_name = "none.";
-
-	weapon_item = get_entity_property(ent, "weapon_item");
-
-	if (weapon_item)
-	{
-		weapon_name = get_entity_property(weapon_item, "name");
-
-		
-	}
-	
-	log("\n weapon_name: " + weapon_name);
+	// Dropping weapon?
+	try_weapon_drop(ent);
 
 	// Release any grappled entities.
 	dc_grapple_release_all(ent);
 }
 
+#define GALFORD_SWORD 1
+#define DROP_THRESHOLD 1
+#define DAMAGE_THRESHOLD 15
+
+// Galford loses his sword under certain conditions.
+void try_weapon_drop(void ent)
+{
+	int  force_drop;                      // Incomming knockdown force.
+	int  force_damage;                    // Incoming damage.
+	int  health = getentityproperty(ent, "health");       //Current HP.
+	float pos_y;
+	float pos_base;
+
+	void weapon_item;
+	int weapon_number;
+
+	// Have a weapon?
+	weapon_item = get_entity_property(ent, "weapon_item");
+
+	if (!weapon_item)
+	{
+		return 0;
+	}
+
+	// Is weapon the one we are looking for?
+	weapon_number = getentityproperty(ent, "weapnum");
+
+	if (weapon_number != GALFORD_SWORD)
+	{
+		return 0;
+	}
 
 
+	// Now let's look at conditions. 
+
+	force_drop = getlocalvar("drop");                      
+
+	if (force_drop < DROP_THRESHOLD)
+	{
+		return 0;
+	}
+
+	force_damage = getlocalvar("damage");
+
+	pos_y = get_entity_property(ent, "position_y");
+	pos_base = get_entity_property(ent, "position_base");
+
+	// If the damage is less than our threshold AND we're
+	// on the ground, then return false.
+	if (force_damage < DAMAGE_THRESHOLD && dc_math_difference_float(pos_y, pos_base) <= 0.1)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+#undef GALFORD_SWORD
+#undef DROP_THRESHOLD
+#undef DAMAGE_THRESHOLD
--- data/scripts/common/event_damage.c
+++ data/scripts/common/event_damage.c
@@ -1,9 +1,179 @@
 #include "data/scripts/dc_elmers/main.c"
 #include "data/scripts/dc_fidelity/main.c"
 
-#define HP_LOW 0.25
+
 
 void main()
+{
+	void ent = getlocalvar("self");
+
+	common_take_damage();
+
+	// Release any grappled entities.
+	dc_elmers_set_target(ent);
+	dc_elmers_disrupt_grapple();
+}
+
+#define DC_DAMAGE_HP_HIGH 0.75
+#define DC_DAMAGE_HP_KO 0.0
+#define DC_DAMAGE_HP_LOW 0.25
+#define DC_DAMAGE_HP_MAX 1.0
+
+#define DC_DAMAGE_TAKEDAMAGE_BURN_ADDITION			0 
+#define DC_DAMAGE_TAKEDAMAGE_BURN_INITIAL			1
+#define DC_DAMAGE_TAKEDAMAGE_HP_HIGH				2
+#define DC_DAMAGE_TAKEDAMAGE_HP_MAX					4
+#define DC_DAMAGE_TAKEDAMAGE_HP_KO					8
+#define DC_DAMAGE_TAKEDAMAGE_HP_LOW					16
+#define DC_DAMAGE_TAKEDAMAGE_KNOCKDOWN_ADDITION		32
+#define DC_DAMAGE_TAKEDAMAGE_KNOCKDOWN_INITIAL		64
+#define DC_DAMAGE_TAKEDAMAGE_LANDING				128	
+#define DC_DAMAGE_TAKEDAMAGE_SHOCK_ADDITION			256
+#define DC_DAMAGE_TAKEDAMAGE_SHOCK_INITIAL			512
+
+// Caskey, Damon V.
+// 2019-10-02
+// 
+// Compute result of damage taking.
+// Knock us down, burn us, kill us, etc.
+// That way we can decide what sounds to 
+// play and possibly take other actions too.
+void common_take_damage_result(void ent)
+{
+	int animation_id;
+	int animation_frame;
+	int landframe;
+	int drop;
+	int attack_type;
+	float hp;
+	float hp_max;
+	float hp_ratio;
+	int result = 0;
+
+
+	// HP result. We get max and current HP, find the percentage
+	// of current vs. high, and then set flag based on
+	// preset thresholds.
+	hp = 0.0 + get_entity_property(ent, "hp");
+	hp_max = 0.0 + getentityproperty(ent, "maxhealth");
+
+	hp_ratio = hp / hp_max;
+
+	if (hp_ratio <= 0)
+	{
+		result += DC_DAMAGE_TAKEDAMAGE_HP_KO;
+	}
+	else if (hp_ratio <= DC_DAMAGE_HP_LOW)
+	{
+		result += DC_DAMAGE_TAKEDAMAGE_HP_LOW;
+	}
+	else if (hp_ratio < DC_DAMAGE_HP_MAX)
+	{
+		result += DC_DAMAGE_TAKEDAMAGE_HP_HIGH;
+	}
+	else
+	{
+		result += DC_DAMAGE_TAKEDAMAGE_HP_MAX;
+	}
+
+	// Is this landing damage?
+	animation_frame = get_entity_property(ent, "animation_frame");
+	landframe = getentityproperty(ent, "landframe", animation_id);
+
+	if (animation_frame == landframe)
+	{
+		result += DC_DAMAGE_TAKEDAMAGE_LANDING;
+	}
+
+	// Knockdown hit?
+	drop = get_entity_property(ent, "drop");
+
+
+
+	// If the attack will KO us, we go right to
+	// KO sound. Otherwise things get more complex.
+	if (hp > 0)
+	{
+		animation_id = get_entity_property(ent, "animation_id");
+		animation_frame = get_entity_property(ent, "animation_frame");
+		drop = get_entity_property(ent, "drop");
+
+		landframe = getentityproperty(ent, "landframe", animation_id);
+
+		// Not landing?
+		if (animation_frame != landframe)
+		{
+			// Knockdown hit?
+			if (drop > 0)
+			{
+				attack_type = getlocalvar("attacktype");
+
+				// We want to know if this is a burn or shock hit, in which
+				// case we also want to know if we're not already in the burn
+				// or shock animaiton. In those cases, we play the burn or shock
+				// voice.
+				//
+				// Otherwise this is a standard knockdown, so we'll react accordingly.
+
+				if (attack_type == openborconstant("ATK_BURN") && animation_id != openborconstant("ANI_BURN"))
+				{
+					dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_PAIN_BURN);
+				}
+				else if (attack_type == openborconstant("ATK_SHOCK") && animation_id != openborconstant("ANI_SHOCK"))
+				{
+					dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_PAIN_SHOCK);
+				}
+				else
+				{
+					// This is a normal knockdown, or a burn/shock but we're already 
+					// in the burn/shock animation (we don't want to do the dramatic 
+					// burn/shock scream over and over). Are our hitpoints low? Decide
+					// if we'll play a normal pain grunt or the low health voice
+					// accordingly.
+
+					hp_max = 0.0 + getentityproperty(ent, "maxhealth");
+
+					if ((hp / hp_max) <= DC_DAMAGE_HP_KO)
+					{
+						dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_NEAR_KO);
+					}
+					else
+					{
+						dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_PAIN_FALL);
+					}
+				}
+			}
+			else
+			{
+				// Non knockdown hit.
+				dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_PAIN);
+			}
+		}
+		else
+		{
+			// We're being damaged by landing on the ground. Are our hitpoints 
+			// low? Decide if we'll play a normal pain grunt or the low health 
+			// voice accordingly.
+
+			hp_max = 0.0 + getentityproperty(ent, "maxhealth");
+
+			if ((hp / hp_max) <= DC_DAMAGE_HP_KO)
+			{
+				// Play HP low voice.
+			}
+			else
+			{
+				// Standard pain voice.
+			}
+		}
+	}
+	else
+	{
+		// KO!
+	}
+}
+
+void common_take_damage()
 {
 	void ent;
 	int animation_id;
@@ -15,10 +185,6 @@ void main()
 	float hp_max;
 
 	ent = getlocalvar("self");
-
-	// Release any grappled entities.
-	dc_elmers_set_target(ent);
-	dc_elmers_disrupt_grapple();
 
 	hp = 0.0 + get_entity_property(ent, "hp");
 
@@ -65,7 +231,7 @@ void main()
 
 					hp_max = 0.0 + getentityproperty(ent, "maxhealth");
 
-					if ((hp / hp_max) <= HP_LOW)
+					if ((hp / hp_max) <= DC_DAMAGE_HP_KO)
 					{
 						dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_NEAR_KO);
 					}
@@ -89,7 +255,7 @@ void main()
 			
 			hp_max = 0.0 + getentityproperty(ent, "maxhealth");
 
-			if ((hp / hp_max) <= HP_LOW)
+			if ((hp / hp_max) <= DC_DAMAGE_HP_KO)
 			{
 				// Play HP low voice.
 				dc_fidelity_quick_play(DC_FIDELITY_TYPE_VOICE_NEAR_KO);
